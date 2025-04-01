81f18eaf6674c052fcca40f914e9ea21840eb6c9
ChennyToo <kchencd1@gmail.com>
2025-04-01 17:12:28 -0400

    .arcgis folder made for pom dependenencies related to ArcGIS SDK

--- demographiq-server/src/main/java/com/demographiq/service/ArcGISService.java
+++ demographiq-server/src/main/java/com/demographiq/service/ArcGISService.java
@@ -8,7 +8,7 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class ArcGISService {
