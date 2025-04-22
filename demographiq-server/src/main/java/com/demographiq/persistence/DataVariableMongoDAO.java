e85068b5beed4f3779409a971416e8192686ba42
ChennyToo <kchencd1@gmail.com>
2025-04-21 23:44:40 -0400

    mongo successfully connects with codebase

--- demographiq-server/src/main/java/com/demographiq/persistence/DataVariableMongoDAO.java
+++ demographiq-server/src/main/java/com/demographiq/persistence/DataVariableMongoDAO.java
@@ -1,5 +1,28 @@
-// package com.demographiq.persistence;
+package com.demographiq.persistence;
 
-// public class DataVariableMongoDAO implements DataVariableDAO {
-    
-// }
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+public class DataVariableMongoDAO {
+
+    public static void main(String[] args) {
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://username:password@demographiq.cbhbhvx.mongodb.net/?retryWrites=true&w=majority&appName=Demographiq";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+
+            // Retrieve all documents in the collection
+            try (MongoCursor<Document> cursor = collection.find().iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            }
+        }
+    }
+}
